dd7d9f28b980af9de58f5c49a8007ba063c9e0ea
piirezrosa <ryanvictorpiresrosa2@gmail.com>
2025-09-02 12:53:13 -0300

    fix: correção de bugs na tela de consulta de produtos

--- StockManager/FrmConsultProducts.Designer.cs
+++ StockManager/FrmConsultProducts.Designer.cs
@@ -31,12 +31,6 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConsultProducts));
             this.DgvConsultProducts = new System.Windows.Forms.DataGridView();
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.produtoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.quantidadeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataFabDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataValDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataRecebDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.registroProdutoBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.cJ3027597PR2DataSet = new StockManager.CJ3027597PR2DataSet();
             this.LblConsultProducts = new System.Windows.Forms.Label();
@@ -46,6 +40,13 @@
             this.TxbBuscarNome = new System.Windows.Forms.TextBox();
             this.PbxConsultProducts = new System.Windows.Forms.PictureBox();
             this.registroProdutoTableAdapter = new StockManager.CJ3027597PR2DataSetTableAdapters.RegistroProdutoTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.produtoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantidadeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataFabDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataValDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataRecebDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Excluir = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.DgvConsultProducts)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.registroProdutoBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cJ3027597PR2DataSet)).BeginInit();
@@ -55,6 +56,7 @@
             // 
             // DgvConsultProducts
             // 
+            this.DgvConsultProducts.AllowUserToAddRows = false;
             this.DgvConsultProducts.AutoGenerateColumns = false;
             this.DgvConsultProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DgvConsultProducts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
@@ -63,12 +65,89 @@
             this.quantidadeDataGridViewTextBoxColumn,
             this.dataFabDataGridViewTextBoxColumn,
             this.dataValDataGridViewTextBoxColumn,
-            this.dataRecebDataGridViewTextBoxColumn});
+            this.dataRecebDataGridViewTextBoxColumn,
+            this.Excluir});
             this.DgvConsultProducts.DataSource = this.registroProdutoBindingSource;
+            this.DgvConsultProducts.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.DgvConsultProducts.Location = new System.Drawing.Point(21, 17);
             this.DgvConsultProducts.Name = "DgvConsultProducts";
-            this.DgvConsultProducts.Size = new System.Drawing.Size(747, 191);
+            this.DgvConsultProducts.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DgvConsultProducts.Size = new System.Drawing.Size(798, 191);
             this.DgvConsultProducts.TabIndex = 0;
+            this.DgvConsultProducts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvConsultProducts_CellContentClick);
+            this.DgvConsultProducts.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvConsultProducts_CellValueChanged);
+            // 
+            // registroProdutoBindingSource
+            // 
+            this.registroProdutoBindingSource.DataMember = "RegistroProduto";
+            this.registroProdutoBindingSource.DataSource = this.cJ3027597PR2DataSet;
+            // 
+            // cJ3027597PR2DataSet
+            // 
+            this.cJ3027597PR2DataSet.DataSetName = "CJ3027597PR2DataSet";
+            this.cJ3027597PR2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // LblConsultProducts
+            // 
+            this.LblConsultProducts.AutoSize = true;
+            this.LblConsultProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblConsultProducts.Location = new System.Drawing.Point(368, 9);
+            this.LblConsultProducts.Name = "LblConsultProducts";
+            this.LblConsultProducts.Size = new System.Drawing.Size(209, 24);
+            this.LblConsultProducts.TabIndex = 1;
+            this.LblConsultProducts.Text = "Consulta de produtos";
+            // 
+            // BtnRechargeConsultProducts
+            // 
+            this.BtnRechargeConsultProducts.Location = new System.Drawing.Point(824, 101);
+            this.BtnRechargeConsultProducts.Name = "BtnRechargeConsultProducts";
+            this.BtnRechargeConsultProducts.Size = new System.Drawing.Size(75, 23);
+            this.BtnRechargeConsultProducts.TabIndex = 2;
+            this.BtnRechargeConsultProducts.Text = "Recarregar";
+            this.BtnRechargeConsultProducts.UseVisualStyleBackColor = true;
+            // 
+            // PnlConsultProducts
+            // 
+            this.PnlConsultProducts.BackColor = System.Drawing.SystemColors.Control;
+            this.PnlConsultProducts.Controls.Add(this.BtnBuscar);
+            this.PnlConsultProducts.Controls.Add(this.DgvConsultProducts);
+            this.PnlConsultProducts.Controls.Add(this.TxbBuscarNome);
+            this.PnlConsultProducts.Controls.Add(this.BtnRechargeConsultProducts);
+            this.PnlConsultProducts.Location = new System.Drawing.Point(12, 36);
+            this.PnlConsultProducts.Name = "PnlConsultProducts";
+            this.PnlConsultProducts.Size = new System.Drawing.Size(902, 245);
+            this.PnlConsultProducts.TabIndex = 3;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(127, 214);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.BtnBuscar.TabIndex = 6;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // TxbBuscarNome
+            // 
+            this.TxbBuscarNome.Location = new System.Drawing.Point(21, 214);
+            this.TxbBuscarNome.Name = "TxbBuscarNome";
+            this.TxbBuscarNome.Size = new System.Drawing.Size(100, 20);
+            this.TxbBuscarNome.TabIndex = 5;
+            // 
+            // PbxConsultProducts
+            // 
+            this.PbxConsultProducts.Image = ((System.Drawing.Image)(resources.GetObject("PbxConsultProducts.Image")));
+            this.PbxConsultProducts.Location = new System.Drawing.Point(786, 303);
+            this.PbxConsultProducts.Name = "PbxConsultProducts";
+            this.PbxConsultProducts.Size = new System.Drawing.Size(215, 225);
+            this.PbxConsultProducts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PbxConsultProducts.TabIndex = 4;
+            this.PbxConsultProducts.TabStop = false;
+            // 
+            // registroProdutoTableAdapter
+            // 
+            this.registroProdutoTableAdapter.ClearBeforeFill = true;
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -113,77 +192,12 @@
             this.dataRecebDataGridViewTextBoxColumn.Name = "dataRecebDataGridViewTextBoxColumn";
             this.dataRecebDataGridViewTextBoxColumn.Width = 110;
             // 
-            // registroProdutoBindingSource
+            // Excluir
             // 
-            this.registroProdutoBindingSource.DataMember = "RegistroProduto";
-            this.registroProdutoBindingSource.DataSource = this.cJ3027597PR2DataSet;
-            // 
-            // cJ3027597PR2DataSet
-            // 
-            this.cJ3027597PR2DataSet.DataSetName = "CJ3027597PR2DataSet";
-            this.cJ3027597PR2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // LblConsultProducts
-            // 
-            this.LblConsultProducts.AutoSize = true;
-            this.LblConsultProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblConsultProducts.Location = new System.Drawing.Point(368, 9);
-            this.LblConsultProducts.Name = "LblConsultProducts";
-            this.LblConsultProducts.Size = new System.Drawing.Size(209, 24);
-            this.LblConsultProducts.TabIndex = 1;
-            this.LblConsultProducts.Text = "Consulta de produtos";
-            // 
-            // BtnRechargeConsultProducts
-            // 
-            this.BtnRechargeConsultProducts.Location = new System.Drawing.Point(774, 100);
-            this.BtnRechargeConsultProducts.Name = "BtnRechargeConsultProducts";
-            this.BtnRechargeConsultProducts.Size = new System.Drawing.Size(75, 23);
-            this.BtnRechargeConsultProducts.TabIndex = 2;
-            this.BtnRechargeConsultProducts.Text = "Recarregar";
-            this.BtnRechargeConsultProducts.UseVisualStyleBackColor = true;
-            // 
-            // PnlConsultProducts
-            // 
-            this.PnlConsultProducts.BackColor = System.Drawing.SystemColors.Control;
-            this.PnlConsultProducts.Controls.Add(this.BtnBuscar);
-            this.PnlConsultProducts.Controls.Add(this.DgvConsultProducts);
-            this.PnlConsultProducts.Controls.Add(this.TxbBuscarNome);
-            this.PnlConsultProducts.Controls.Add(this.BtnRechargeConsultProducts);
-            this.PnlConsultProducts.Location = new System.Drawing.Point(12, 36);
-            this.PnlConsultProducts.Name = "PnlConsultProducts";
-            this.PnlConsultProducts.Size = new System.Drawing.Size(861, 245);
-            this.PnlConsultProducts.TabIndex = 3;
-            // 
-            // BtnBuscar
-            // 
-            this.BtnBuscar.Location = new System.Drawing.Point(127, 214);
-            this.BtnBuscar.Name = "BtnBuscar";
-            this.BtnBuscar.Size = new System.Drawing.Size(75, 23);
-            this.BtnBuscar.TabIndex = 6;
-            this.BtnBuscar.Text = "Buscar";
-            this.BtnBuscar.UseVisualStyleBackColor = true;
-            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
-            // 
-            // TxbBuscarNome
-            // 
-            this.TxbBuscarNome.Location = new System.Drawing.Point(21, 214);
-            this.TxbBuscarNome.Name = "TxbBuscarNome";
-            this.TxbBuscarNome.Size = new System.Drawing.Size(100, 20);
-            this.TxbBuscarNome.TabIndex = 5;
-            // 
-            // PbxConsultProducts
-            // 
-            this.PbxConsultProducts.Image = ((System.Drawing.Image)(resources.GetObject("PbxConsultProducts.Image")));
-            this.PbxConsultProducts.Location = new System.Drawing.Point(786, 303);
-            this.PbxConsultProducts.Name = "PbxConsultProducts";
-            this.PbxConsultProducts.Size = new System.Drawing.Size(215, 225);
-            this.PbxConsultProducts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.PbxConsultProducts.TabIndex = 4;
-            this.PbxConsultProducts.TabStop = false;
-            // 
-            // registroProdutoTableAdapter
-            // 
-            this.registroProdutoTableAdapter.ClearBeforeFill = true;
+            this.Excluir.HeaderText = "Excluir";
+            this.Excluir.Name = "Excluir";
+            this.Excluir.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Excluir.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // FrmConsultProducts
             // 
@@ -227,5 +241,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataFabDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataValDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataRecebDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewButtonColumn Excluir;
     }
 }
--- StockManager/FrmConsultProducts.cs
+++ StockManager/FrmConsultProducts.cs
@@ -13,6 +13,8 @@ namespace StockManager
 {
     public partial class FrmConsultProducts : Form
     {
+        private string connectionString =
+                "Data Source=sqlexpress;Initial Catalog=CJ3027597PR2;User Id=aluno;Password=aluno;";
         public FrmConsultProducts()
         {
             InitializeComponent();
@@ -42,8 +44,7 @@ namespace StockManager
 
         private void CarregarProdutos(string nome = "")
         {
-            string connectionString =
-                "Data Source=sqlexpress;Initial Catalog=CJ3027597PR2;User Id=aluno;Password=aluno;";
+            
 
             using (SqlConnection conexao = new SqlConnection(connectionString))
             {
@@ -67,6 +68,72 @@ namespace StockManager
                 catch (Exception ex)
                 {
                     MessageBox.Show("Erro ao carregar produtos: " + ex.Message);
+                }
+            }
+        }
+
+        private void DgvConsultProducts_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Pega a linha alterada
+            if (e.RowIndex >0 ) {
+            DataGridViewRow row = DgvConsultProducts.Rows[e.RowIndex];
+
+                int id = int.Parse(row.Cells[0].Value.ToString());
+                string produto = row.Cells[1].Value.ToString();
+                int quantidade = int.Parse(row.Cells[2].Value.ToString());
+                DateTime dataFab = DateTime.Parse(row.Cells[3].Value.ToString());
+                DateTime dataVal = DateTime.Parse(row.Cells[4].Value.ToString());
+                DateTime dataReceb = DateTime.Parse(row.Cells[5].Value.ToString());
+
+                // Atualiza o banco
+                string query = @"UPDATE RegistroProduto 
+                     SET Produto = @Produto, Quantidade = @Quantidade, DataFab = @DataFab, DataVal = @DataVal, DataReceb = @DataReceb
+                     WHERE Id = @Id";
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", id);
+                        cmd.Parameters.AddWithValue("@Produto", produto);
+                        cmd.Parameters.AddWithValue("@Quantidade", quantidade);
+                        cmd.Parameters.AddWithValue("@DataFab", dataFab);
+                        cmd.Parameters.AddWithValue("@DataVal", dataVal);
+                        cmd.Parameters.AddWithValue("@DataReceb", dataReceb);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Produto atualizado com sucesso!");
+
+            }
+        }
+
+        private void DgvConsultProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (DgvConsultProducts.Columns[e.ColumnIndex] is DataGridViewButtonColumn && e.RowIndex >= 0)
+            {
+                string produto = DgvConsultProducts.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+                DialogResult result = MessageBox.Show($"Deseja excluir o produto {produto}?", "Confirmar exclusão", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    // Remove do banco
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        string query = "DELETE FROM RegistroProduto WHERE Produto = @Produto";
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@Produto", produto);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    // Remove da grid
+                    DgvConsultProducts.Rows.RemoveAt(e.RowIndex);
+                    MessageBox.Show("Produto excluído com sucesso!");
                 }
             }
         }
